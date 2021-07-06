f06e12588a2df891527664157d4a1d90ebc346cc
zaba505 <cakub6@gmx.com>
2021-07-06 07:17:09 -0400

    feat(store): added libp2p node config state

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import {
   createWebHashHistory,
   createWebHistory,
 } from 'vue-router';
-import { StateInterface } from '../store';
+import { State } from '../store';
 import routes from './routes';
 
 /*
@@ -17,7 +17,7 @@ import routes from './routes';
  * with the Router instance.
  */
 
-export default route<StateInterface>(function (/* { store, ssrContext } */) {
+export default route<State>(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
