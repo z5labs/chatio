5a368efd7eb5a684e8d1bafee7fc4201bc6be751
zaba505 <cakub6@gmx.com>
2021-07-06 11:46:48 -0400

    feat(call): integrate chat into call layout

--- src/layouts/CallLayout.vue
+++ src/layouts/CallLayout.vue
@@ -21,6 +21,16 @@
       </q-page>
     </q-page-container>
 
+    <q-drawer
+      v-model="showChat"
+      side="right"
+      :width="300"
+      :breakpoint="500"
+      elevated
+    >
+      <Chat @message="sendMessage" :messages="messages" />
+    </q-drawer>
+
     <q-footer bordered class="bg-black text-primary">
       <q-toolbar>
         <q-checkbox v-model="audio" label="Audio" color="primary" keep-color />
@@ -29,7 +39,7 @@
         <q-space />
 
         <q-btn icon="people" label="Participants" flat />
-        <q-btn icon="message" label="Chat" flat />
+        <q-btn @click="showChat = !showChat" icon="message" label="Chat" flat />
       </q-toolbar>
     </q-footer>
   </q-layout>
@@ -37,12 +47,14 @@
 
 <script lang="ts">
 import { defineAsyncComponent, defineComponent, ref, Ref } from 'vue';
+import { Message } from 'src/components/model';
 
 export default defineComponent({
   name: 'CallLayout',
   components: {
     Libp2p: defineAsyncComponent(() => import('components/Libp2p.vue')),
     Call: defineAsyncComponent(() => import('components/Call.vue')),
+    Chat: defineAsyncComponent(() => import('components/Chat.vue'))
   },
   props: {
     id: {
@@ -57,12 +69,27 @@ export default defineComponent({
     const node: Ref<Record<string, unknown> | null> = ref(null);
     const audio = ref(false);
     const video = ref(false);
+    const showChat = ref(false);
+    const messages = ref<Message[]>([]);
 
     const setNode = (n: Record<string, unknown>) => node.value = n;
+
+    const sendMessage = (body: string) => {
+      const timestamp = Date.now();
+
+      messages.value.push({
+        user: 'Me',
+        body,
+        timestamp
+      })
+    };
 
     return {
       audio,
       video,
+      showChat,
+      messages,
+      sendMessage,
       setNode,
     };
   }
