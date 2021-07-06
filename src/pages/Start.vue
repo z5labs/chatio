54da5362d2dccfc74e62c8410300e974f8bbd41f
Zaba505 <cakub6@gmx.com>
2021-07-06 10:34:24 -0400

    feat(call): can now start a call with a unique id (#13)

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,16 +1,38 @@
 <template>
-  <q-page>
-    Start
+  <q-page class="row items-center justify-center">
+    <q-btn @click="startCall" color="primary">Start</q-btn>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'Start',
   setup() {
-    return {};
+    const router = useRouter();
+
+    const startCall = async () => {
+      // Generate call id from crptographically random values and SHA-256
+      const rvals = crypto.getRandomValues(new Uint8Array(64));
+      const buf = await crypto.subtle.digest('SHA-256', rvals.buffer);
+      const hashArray = Array.from(new Uint8Array(buf));
+      const id = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+      const query = {
+        id,
+      };
+
+      void router.push({
+        path: '/call',
+        query: query,
+      });
+    };
+
+    return {
+      startCall
+    };
   }
 })
 </script>
