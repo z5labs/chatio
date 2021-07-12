d67512cf42ec4154531f93ee9987284e1f84104f
zaba505 <cakub6@gmx.com>
2021-07-12 04:10:31 -0400

    feat(discovery): use webrtc-star over bootstrap

--- src/components/libp2p.js
+++ src/components/libp2p.js
@@ -1,20 +1,16 @@
 /* eslint-disable */
 
-// Libp2p Core
 import Libp2p from 'libp2p';
-// Transports
 import Websockets from 'libp2p-websockets';
 import filters from 'libp2p-websockets/src/filters';
 import WebrtcStar from 'libp2p-webrtc-star';
-// Stream Muxer
 import Mplex from 'libp2p-mplex';
-// Connection Encryption
 import { NOISE } from 'libp2p-noise';
-// Peer Discovery
-import Bootstrap from 'libp2p-bootstrap';
 import KadDHT from 'libp2p-kad-dht';
-// Gossipsub
 import Gossipsub from 'libp2p-gossipsub';
+import debug from 'debug';
+
+const log = debug('chatio:peer');
 
 const transportKey = Websockets.prototype[Symbol.toStringTag];
 
@@ -28,14 +24,14 @@ export const createLibp2p = async (config) => {
       transport: [Websockets, WebrtcStar],
       streamMuxer: [Mplex],
       connEncryption: [NOISE],
-      peerDiscovery: [Bootstrap],
       dht: KadDHT,
       pubsub: Gossipsub
     },
     config: {
       peerDiscovery: {
-        bootstrap: {
-          list: config.bootstrapAddrs,
+        autoDial: true,
+        [WebrtcStar.tag]: {
+          enabled: true,
         }
       },
       dht: {
@@ -53,6 +49,10 @@ export const createLibp2p = async (config) => {
       }
     }
   });
+
+  node.connectionManager.on('peer:discovery', peerId => log(`Discovered: ${peerId.toB58String()}`));
+  node.connectionManager.on('peer:connect', conn => log(`Connected: ${conn.remotePeer.toB58String()}`));
+  node.connectionManager.on('peer:disconnect', conn => log(`Disconnected: ${conn.remotePeer.toB58String()}`));
 
   await node.start();
 
