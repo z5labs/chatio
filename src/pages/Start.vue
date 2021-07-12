214ee5c31f75dac24b77f179f0aec015adb707fa
zaba505 <cakub6@gmx.com>
2021-07-12 14:55:30 -0400

    feat(call): can now start a call with custom id

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,27 +1,30 @@
 <template>
-  <q-page class="row items-center justify-center">
+  <q-page class="column items-center justify-center">
+    <q-input v-model="id" label="Call ID" clearable />
     <q-btn @click="startCall" color="primary">Start</q-btn>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'Start',
   setup() {
     const router = useRouter();
+    const id = ref('');
 
-    const startCall = async () => {
-      // Generate call id from crptographically random values and SHA-256
-      const rvals = crypto.getRandomValues(new Uint8Array(64));
-      const buf = await crypto.subtle.digest('SHA-256', rvals.buffer);
+    // Generate call id from crptographically random values and SHA-256
+    const rvals = crypto.getRandomValues(new Uint8Array(64));
+    void crypto.subtle.digest('SHA-256', rvals.buffer).then(buf => {
       const hashArray = Array.from(new Uint8Array(buf));
-      const id = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+      id.value = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    });
 
+    const startCall = () => {
       const query = {
-        id,
+        id: id.value,
       };
 
       void router.push({
@@ -31,6 +34,7 @@ export default defineComponent({
     };
 
     return {
+      id,
       startCall
     };
   }
