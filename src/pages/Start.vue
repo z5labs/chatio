71f445b7e384f38e776f62045d277fb879c951be
zaba505 <cakub6@gmx.com>
2021-07-13 02:12:27 -0400

    feat(call): add validation rules for call id field

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,7 +1,27 @@
 <template>
   <q-page class="column items-center justify-center">
-    <q-input v-model="id" label="Call ID" clearable />
-    <q-btn @click="startCall" color="primary">Start</q-btn>
+    <div class="q-pa-md">
+      <q-form
+        @submit="startCall"
+        @reset="resetCallID"
+        class="q-gutter-md"
+      >
+        <q-input
+          v-model="id"
+          label="Call ID"
+          hint="By modifying this you trade security for convenience."
+          hide-hint
+          clearable
+          lazy-rules
+          :rules="[ val => val && val.trim().length > 0 || 'Call ID must not be empty']"
+        />
+
+        <div>
+          <q-btn :disable="!id || id.trim().length === 0" type="submit" color="primary">start</q-btn>
+          <q-btn type="reset" color="secondary" flat>regenerate</q-btn>
+        </div>
+      </q-form>
+    </div>
   </q-page>
 </template>
 
@@ -33,9 +53,17 @@ export default defineComponent({
       });
     };
 
+    const resetCallID = async () => {
+      const rvals = crypto.getRandomValues(new Uint8Array(64));
+      const buf = await crypto.subtle.digest('SHA-256', rvals.buffer);
+      const hashArray = Array.from(new Uint8Array(buf));
+      id.value = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    };
+
     return {
       id,
-      startCall
+      startCall,
+      resetCallID
     };
   }
 })
