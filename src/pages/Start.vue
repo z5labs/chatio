dbf2433a5e6c74d3f768a9fd8c27c84bf830904a
Zaba505 <cakub6@gmx.com>
2021-07-13 02:12:56 -0400

    feat(call): can now start a call with custom id (#34)
    * feat(call): can now start a call with custom id
    
    * feat(call): add validation rules for call id field

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,27 +1,50 @@
 <template>
-  <q-page class="row items-center justify-center">
-    <q-btn @click="startCall" color="primary">Start</q-btn>
+  <q-page class="column items-center justify-center">
+    <div class="q-pa-md">
+      <q-form
+        @submit="startCall"
+        @reset="resetCallID"
+        class="q-gutter-md"
+      >
+        <q-input
+          v-model="id"
+          label="Call ID"
+          hint="By modifying this you trade security for convenience."
+          hide-hint
+          clearable
+          lazy-rules
+          :rules="[ val => val && val.trim().length > 0 || 'Call ID must not be empty']"
+        />
+
+        <div>
+          <q-btn :disable="!id || id.trim().length === 0" type="submit" color="primary">start</q-btn>
+          <q-btn type="reset" color="secondary" flat>regenerate</q-btn>
+        </div>
+      </q-form>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'Start',
   setup() {
     const router = useRouter();
+    const id = ref('');
 
-    const startCall = async () => {
-      // Generate call id from crptographically random values and SHA-256
-      const rvals = crypto.getRandomValues(new Uint8Array(64));
-      const buf = await crypto.subtle.digest('SHA-256', rvals.buffer);
+    // Generate call id from crptographically random values and SHA-256
+    const rvals = crypto.getRandomValues(new Uint8Array(64));
+    void crypto.subtle.digest('SHA-256', rvals.buffer).then(buf => {
       const hashArray = Array.from(new Uint8Array(buf));
-      const id = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+      id.value = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    });
 
+    const startCall = () => {
       const query = {
-        id,
+        id: id.value,
       };
 
       void router.push({
@@ -30,8 +53,17 @@ export default defineComponent({
       });
     };
 
+    const resetCallID = async () => {
+      const rvals = crypto.getRandomValues(new Uint8Array(64));
+      const buf = await crypto.subtle.digest('SHA-256', rvals.buffer);
+      const hashArray = Array.from(new Uint8Array(buf));
+      id.value = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    };
+
     return {
-      startCall
+      id,
+      startCall,
+      resetCallID
     };
   }
 })
