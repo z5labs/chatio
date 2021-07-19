c654871bbc44f93e0d74d3a6d7b2f1195b7fbccb
zaba505 <cakub6@gmx.com>
2021-07-19 02:16:06 -0400

    refactor(call): remove prop since node is injected

--- src/components/Call.vue
+++ src/components/Call.vue
@@ -1,5 +1,7 @@
 <template>
-  Hello
+  <div class="call">
+    Hello
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,15 +9,23 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Call',
-  props: {
-    node: {
-      type: Object,
-    }
-  },
   setup() {
     return {};
   }
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.call {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr;
+  align-items: center;
+  justify-items: center;
+}
+</style>
