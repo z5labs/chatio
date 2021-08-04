eee42b0b8527e54d48ce1415c2c825a2ec910f94
zaba505 <cakub6@gmx.com>
2021-08-04 03:54:57 -0400

    feat(participants): outline basic component for displaying participants

---
+++ src/components/Participants.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-scroll-area class="fit">
+    <q-list padding separator>
+      <q-item v-for="participant in participants" :key="participant.id.toB58String()">
+        <q-item-section>
+          <q-item-label>{{ participant.id.toB58String() }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-scroll-area>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Participant } from 'src/components/call';
+
+export default defineComponent({
+  name: 'Participants',
+  props: {
+    participants: {
+      type: Array as PropType<Array<Participant>>,
+      required: true
+    }
+  },
+  setup() {
+    return {};
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
